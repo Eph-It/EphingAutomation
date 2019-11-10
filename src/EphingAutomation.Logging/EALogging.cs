cc8e57f2d3e97d11923612dd10fc9404cd17c8d3
Ryan Ephgrave <ryan2065@gmail.com>
2019-11-10 13:57:25 -0600

    --- Auto-Git Commit ---

--- src/EphingAutomation.Logging/EALogging.cs
+++ src/EphingAutomation.Logging/EALogging.cs
@@ -22,7 +22,7 @@ namespace EphingAutomation.Logging
             string path;
             if(_environment != null)
             {
-                path = Path.Combine(_environment?.ContentRootPath ?? _environment?.WebRootPath, "Logs", $"{LogName}.log");
+                path = Path.Combine(_environment.ContentRootPath ?? _environment?.WebRootPath, "Logs", $"{LogName}.log");
             }
             else
             {
