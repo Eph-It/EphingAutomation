044b7cd119ae829f3fdad92eba2666df4787a633
Ryan Ephgrave <ryan2065@gmail.com>
2019-11-10 13:13:39 -0600

    --- Auto-Git Commit ---

--- src/EphingAutomation.CM.StatusMessageProcessorService/BeforeBuild.ps1
+++ src/EphingAutomation.CM.StatusMessageProcessorService/BeforeBuild.ps1
@@ -1,3 +1,4 @@
 $ServiceName = 'EA.CM.StatusMessageProcessorService'
 Get-Service -Name $ServiceName | Stop-Service -Force -ErrorAction SilentlyContinue
+Start-Sleep 10
 #Stop-Service -Name $ServiceName -ErrorAction SilentlyContinue -Force
