e2b164c53791e279e65073384ca80c332c87e1fd
Ryan Ephgrave <ryan2065@gmail.com>
2019-11-10 12:54:53 -0600

    --- Auto-Git Commit ---

--- src/SetupService.ps1
+++ src/SetupService.ps1
@@ -1,7 +1,7 @@
 $ServiceName = 'EA.CM.StatusMessageProcessorService'
 $Service = Get-CimInstance -Query  "Select * FROM Win32_Service where Name like '$ServiceName'"
 #C:\Users\Ryan2\source\repos\EphingAutomation\src\EphingAutomation.CM.StatusMessageProcessorService\bin\debug\netcoreapp3.0\EphingAutomation.CM.StatusMessageProcessorService.exe
-$BinaryPath = "$PSScriptRoot\EphingAutomation.CM.StatusMessageProcessorService\bin\debug\netcoreapp3.0\EphingAutomation.CM.StatusMessageProcessorService.exe"
+$BinaryPath = "$PSScriptRoot\EphingAutomation.CM.StatusMessageProcessorService\bin\debug\netcoreapp3.0\win7-x64\EphingAutomation.CM.StatusMessageProcessorService.exe"
 
 if($Service -ne $null) {
     if(-not ( $Service.PathName -contains $BinaryPath )){
