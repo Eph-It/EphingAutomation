43c4be849b008b564e4e6843603474c116f38f3c
Ryan Ephgrave <Ryan2065@gmail.com>
2019-11-11 15:23:41 -0600

    --- Auto-Git Commit ---

--- src/EphingAutomation.CM.StatusMessageDB/StatusMessageDBContext.cs
+++ src/EphingAutomation.CM.StatusMessageDB/StatusMessageDBContext.cs
@@ -1,8 +1,41 @@
 ﻿using System;
+using System.IO;
+using System.Data.SQLite;
+using Dapper;
 
 namespace EphingAutomation.CM.StatusMessageDB
 {
     public class StatusMessageDBContext
     {
+        private string _dbPath;
+        public StatusMessageDBContext(string dbPath)
+        {
+            if (!File.Exists(dbPath))
+            {
+                CreateDB(dbPath);
+            }
+        }
+
+        public SQLiteConnection SqlConnection()
+        {
+            return new SQLiteConnection("Data Source=" + _dbPath);
+        }
+
+        private void CreateDB(string dbPath)
+        {
+            using (var connection = SqlConnection())
+            {
+                connection.Open();
+                connection.Execute(@"
+                    CREATE TABLE StatusMessages
+                    (
+                        ID              integer primary key AUTOINCREMENT,
+                        MESSAGE_ID      integer,
+                        RECORDED        datetime,
+                        
+                    )
+                ");
+            }
+        }
     }
 }
