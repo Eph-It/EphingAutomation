08d2c63256f964d3ac88458db10d2422fa251d76
Ryan Ephgrave <Ryan2065@gmail.com>
2019-11-12 16:27:49 -0600

    --- Auto-Git Commit ---

--- src/EphingAutomation.CM.StatusMessageReceiver/Program.cs
+++ src/EphingAutomation.CM.StatusMessageReceiver/Program.cs
@@ -8,41 +8,66 @@ using Serilog;
 using EphingAutomation.Logging;
 using System.Threading;
 using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using EphingAutomation.CM.StatusMessageDB.Model;
+using EphingAutomation.CM.StatusMessageDB;
 
 namespace EphingAutomation.CM.StatusMessageReceiver
 {
     class Program
     {
-        public static Dictionary<string, string> ProcessArguments(string[] args)
+        public static IConfigurationRoot GetConfiguration(string[] args)
         {
-            var returnDictionary = new Dictionary<string, string>();
-            bool argIsValue = false;
-            for (int i = 0; i < args.Count(); i++)
-            {
-                if (!argIsValue)
-                {
-                    string arg = args[i];
-                    if (!String.IsNullOrEmpty(arg))
-                    {
-                        if (arg.StartsWith('-') || arg.StartsWith('/'))
-                        {
-                            string key = arg.TrimStart('-').TrimStart('-').TrimStart('/');
-                            returnDictionary.Add(key, args[i + 1]);
-                        }
-                    }
-                }
-                else
-                {
-                    argIsValue = false;
-                }
-            }
-            return returnDictionary;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddCommandLine(args);
+
+            return builder.Build();
         }
         static void Main(string[] args)
         {
             var loggerConfig = new EALogging();
             loggerConfig.Configure("StatusMessageReceiver");
-            Log.Information("Started with parameters {@args}", args);
+            // Log.Information("Started with parameters {@args}", args);
+            IConfigurationRoot configuration = null;
+            try
+            {
+                configuration = GetConfiguration(args);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error getting configuration");
+                throw;
+            }
+            var smObject = new StatusMessage();
+            smObject.MessageId = int.Parse(configuration["MessageId"]);
+            smObject.InsString1 = configuration["InsString1"];
+            smObject.InsString2 = configuration["InsString2"];
+            smObject.InsString3 = configuration["InsString3"];
+            smObject.InsString4 = configuration["InsString4"];
+            smObject.InsString5 = configuration["InsString5"];
+            smObject.InsString6 = configuration["InsString6"];
+            smObject.InsString7 = configuration["InsString7"];
+            smObject.InsString8 = configuration["InsString8"];
+            smObject.InsString9 = configuration["InsString9"];
+            smObject.InsString10 = configuration["InsString10"];
+            smObject.Recorded = DateTime.UtcNow;
+
+            string dbPath = configuration["StatusMessageDb"];
+            var db = new StatusMessageDBContext(dbPath);
+
+            var jobRepo = new StatusMessageDB.Repository.JobRepository(db);
+            var smRepo = new StatusMessageDB.Repository.StatusMessageRepository(db);
+
+            smRepo.New(smObject);
+
+            var openJobs = jobRepo.GetActiveJobs();
+            if(openJobs.Count == 0)
+            {
+                // logic to open a job
+            }
+
             /*IServiceRepository serviceRepo = new ServiceRepository();
             IStatusMessageReceiverErrorHandling statMessageErrorHandling = new StatusMessageReceiverErrorHandling();
             var smObject = new StatusMessage()
--- src/EphingAutomation.CM.StatusMessageReceiver/RunLots.ps1
+++ src/EphingAutomation.CM.StatusMessageReceiver/RunLots.ps1
@@ -1,9 +1,13 @@
-$File = "$PSScriptRoot\bin\Debug\netcoreapp3.0\EA.CM.StatusMessageReceiver.exe"
+$File = "$PSScriptRoot\bin\Release\netcoreapp3.0\win7-x64\EA.CM.StatusMessageReceiver.exe"
+Push-Location -Path $PSScriptRoot
+
+#dotnet publish --configuration Release --self-contained
 
 $count = 0
 Measure-Command {
-    while($count -lt 10) {
+    while($count -lt 100) {
         $count++
-        & cmd /c $File "arg$count" "this is a test"
+        & cmd /c $File --MessageId 55 --InsString1 "test1"
     }
 }
+Pop-Location
