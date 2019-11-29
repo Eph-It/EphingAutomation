64cf13ecc997ea2008da84fd5452b0117f0428e9
Ryan Ephgrave <ryan2065@gmail.com>
2019-11-29 16:56:19 -0600

    updated collection

--- src/EphingAutomation.CM.Models/Collection.cs
+++ src/EphingAutomation.CM.Models/Collection.cs
@@ -4,5 +4,16 @@ namespace EphingAutomation.CM.Models
 {
     public class Collection
     {
+        public string Name { get; set; }
+        public string Comment { get; set; }
+        public string LimitingCollectionId { get; set; }
+        public Collection LimitingCollection { get; set; }
+        public string Type { get; set; }
+        public string CollectionId { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public DateTime LastMembershipChange { get; set; }
+        public bool UseIncrementalChanges { get; set; }
+        public bool ScheduleFullUpdate { get; set; }
+
     }
 }
