a520dec2e33488d404f7a905e8a93d0c35fd2891
Ryan Ephgrave <Ryan2065@gmail.com>
2019-11-29 22:37:07 -0600

    updated

---
+++ src/EphingAutomation.CM.StatusMessageProcessor/Repository/StatusMessageJobRepository.cs
@@ -0,0 +1,20 @@
+﻿using EphingAutomation.CM.StatusMessageDB.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EphingAutomation.CM.StatusMessageProcessor.Repository
+{
+    public class StatusMessageJobRepository
+    {
+        StatusMessage _sm;
+        public StatusMessageJobRepository(StatusMessage sm)
+        {
+            _sm = sm;
+        }
+        public void Start()
+        {
+
+        }
+    }
+}
--- src/EphingAutomation.CM.StatusMessageProcessor/Repository/StatusMessageProcessorRepository.cs
+++ src/EphingAutomation.CM.StatusMessageProcessor/Repository/StatusMessageProcessorRepository.cs
@@ -1,9 +1,11 @@
 ﻿using EphingAutomation.CM.StatusMessageDB;
+using EphingAutomation.CM.StatusMessageDB.Model;
 using EphingAutomation.CM.StatusMessageDB.Repository;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace EphingAutomation.CM.StatusMessageProcessor.Repository
 {
@@ -11,27 +13,56 @@ namespace EphingAutomation.CM.StatusMessageProcessor.Repository
     {
         IStatusMessageRepository _smRepo;
         IJobRepository _jobRepository;
-
+        List<Task> _backgroundJobs = new List<Task>();
         public StatusMessageProcessorRepository(StatusMessageDBContext dbContext)
         {
             _smRepo = new StatusMessageRepository(dbContext);
             _jobRepository = new JobRepository(dbContext);
         }
+        private void BackgroundProcess(StatusMessage sm)
+        {
+            var smJobRepo = new StatusMessageJobRepository(sm);
+            var backgroundJob = Task.Run(() => smJobRepo.Start());
+            
+            _backgroundJobs.Add(backgroundJob);
+        }
+        private void RemoveCompletedTasks()
+        {
+            for (int i = 0; i < _backgroundJobs.Count; i++)
+            {
+                var t = _backgroundJobs[i];
+                if (t.IsCompleted)
+                {
+                    _backgroundJobs.Remove(t);
+                }
+            }
+        }
         public void Start()
         {
             _jobRepository.New();
             DateTime LastProcessed = DateTime.UtcNow;
-            while(LastProcessed < DateTime.UtcNow.AddMinutes(5))
+            while (LastProcessed < DateTime.UtcNow.AddMinutes(5))
             {
+                if(_backgroundJobs.Count > 0)
+                {
+                    RemoveCompletedTasks();
+                }
                 var nextSM = _smRepo.GetNext();
-                if(nextSM != null)
+                if (nextSM != null)
                 {
                     LastProcessed = DateTime.UtcNow;
-
+                    BackgroundProcess(nextSM);
                 }
                 Thread.Sleep(100);
             }
             _jobRepository.EndJob();
+            var lastSM = _smRepo.GetNext();
+            if (lastSM != null)
+            {
+                BackgroundProcess(lastSM);
+            }
+            Task.WaitAll(_backgroundJobs.ToArray());
         }
     }
+    
 }
